a93fc17b4792c156352ce4a5e67366bc4fda05dd
antzax <166389859+antzax@users.noreply.github.com>
2025-04-28 23:17:31 +0300

    add test file for mongodb

---
+++ noteapp/notebackend/mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://anton:${password}@cluster0.fxkgccg.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+Note.find({ important: false }).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
+
+// const note = new Note({
+//   content: 'Mongoose makes things easy',
+//   important: true,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
