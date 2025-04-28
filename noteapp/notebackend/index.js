fddf7e0f2db2ba81491a68d46c732cd4d5849eea
antzax <166389859+antzax@users.noreply.github.com>
2025-04-29 00:57:32 +0300

    change routes to use mongoose and configure dotenv

--- noteapp/notebackend/index.js
+++ noteapp/notebackend/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const Note = require('./models/note')
-const app = express()
 
+const app = express()
 app.use(express.static('dist'))
 app.use(express.json())
 
@@ -37,47 +38,32 @@ app.get("/", (req, res) => {
 })
 
 app.get('/api/notes', (req,res) => {
-  Note.find({}).then(notes =>{
+  Note.find({}).then(notes => {
     res.json(notes)
   })
 })
 
 app.get('/api/notes/:id', (req, res) => {
-  const id = req.params.id
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    res.json(note)   
-  } else {
-    res.status(404).end()
-  }
+  Note.findById(req.params.id).then(note => {
+    res.json(note)
+  })
 })
-
-const generateId = () => {
-  const maxId = notes.length > 0 
-  ? Math.max(...notes.map(note => Number(note.id)))
-  : 0
-
-  return String(maxId + 1)
-}
 
 app.post('/api/notes', (req, res) => {
   const body = req.body
 
   if (!body.content) {
-    return res.status(404).json({
-      error: 'content missing'
-    })
+    return res.status(404).json({error: 'content missing'})
   }
 
-  const note = {
-    id: generateId(),
+  const note = new Note({
     content: body.content,
     important: body.important || false
-  }
+  })
 
-  notes = notes.concat(note)
-  res.json(note)
+  note.save().then(note => {
+    res.json(note)
+  })
 })
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -93,7 +79,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
