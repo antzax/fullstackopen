784cf665a66d84a569420f94dba5b03b2498b9b6
antzax <166389859+antzax@users.noreply.github.com>
2025-04-29 20:00:54 +0300

    add phonenumber validation

--- osa3/puhelinluettelo-app/puhelinluettelo-backend/models/person.js
+++ osa3/puhelinluettelo-app/puhelinluettelo-backend/models/person.js
@@ -20,12 +20,12 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     minLength: 8,
-    // validate: {
-    //   validator: function(v) {
-    //     return (/\d{2}-\d{8}/.test(v.trim()) || /\d{3}-\d{7}/.test(v.trim()))
-    //   },
-    //   message: props => `${props.value} is not a valid phone number!`
-    // },
+    validate: {
+      validator: function(v) {
+        return (/\d{2,3}-\d{7,8}/.test(v.trim()))
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
     required: true,
   }
 });
