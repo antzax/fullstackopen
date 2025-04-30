91ccb19d2bc308def4433cf9b21ec4390d34c28e
antzax <166389859+antzax@users.noreply.github.com>
2025-04-30 13:49:11 +0300

    add rest of the tests for notes

--- noteapp/notebackend/controllers/notes.js
+++ noteapp/notebackend/controllers/notes.js
@@ -6,55 +6,50 @@ notesRouter.get('/', async (req,res) => {
   res.json(notes)
 })
 
-notesRouter.get('/:id', (req, res, next) => {
-  Note.findById(req.params.id).then(note => {
-    if(note) {
-      res.json(note)
-    } else {
-      res.status(404).end()
-    }
-  })
-    .catch(error => next(error))
+notesRouter.get('/:id', async (req, res) => {
+  const note = await Note.findById(req.params.id)
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
 })
 
-notesRouter.post('/', async (req, res, next) => {
+notesRouter.post('/', async (req, res) => {
   const body = req.body
 
   const note = new Note({
     content: body.content,
     important: body.important || false
   })
+  
+  const savedNote = await note.save()
+  res.status(201).json(savedNote)
+})
+
+notesRouter.delete('/:id', async (req, res) => {
+  await Note.findByIdAndDelete(req.params.id)
+  res.status(204).end()
+})
+
+notesRouter.put('/:id', async (req, res, next) => {
+  const { content, important } = req.body
+
+  const noteToEdit = await Note.findById(req.params.id)
+
+  if (!noteToEdit) {
+    return res.status(400).end()
+  }
+
+  noteToEdit.content = content
+  noteToEdit.important = important
+
   try {
-    const savedNote = await note.save()
-    res.status(201).json(savedNote)
+    const updatedNote = await noteToEdit.save()
+    res.status(200).json(updatedNote)
   } catch (exception) {
     next(exception)
   }
-})
-
-notesRouter.delete('/:id', (req, res, next) => {
-  Note.findByIdAndDelete(req.params.id).then(() => {
-    res.status(204).end()
-  })
-    .catch(error => next(error) )
-})
-
-notesRouter.put('/:id', (req, res, next) => {
-  const { content, important } = req.body
-
-  Note.findById(req.params.id).then(note => {
-    if (!note) {
-      return res.status(400).end()
-    }
-
-    note.content = content
-    note.important = important
-
-    return note.save().then(updatedNote => {
-      res.status(200).json(updatedNote)
-    })
-  })
-    .catch(error => next(error))
 })
 
 module.exports = notesRouter
