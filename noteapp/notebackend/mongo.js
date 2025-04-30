cf31f9ed5ced9263af76615f61ecf48b0f6e4c24
antzax <166389859+antzax@users.noreply.github.com>
2025-04-30 12:25:37 +0300

    modify for testing

--- noteapp/notebackend/mongo.js
+++ noteapp/notebackend/mongo.js
@@ -7,8 +7,9 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://anton:${password}@cluster0.fxkgccg.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://anton:${password}@cluster0.fxkgccg.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0`
 
+console.log('connecting to mongodb:', url)
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
@@ -19,19 +20,23 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
+if(process.argv.length === 3){
+  Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
   })
-  mongoose.connection.close()
-})
+}
 
-// const note = new Note({
-//   content: 'Mongoose makes things easy',
-//   important: true,
-// })
+if(process.argv.length === 5){
+  const note = new Note({
+    content: process.argv[3],
+    important: process.argv[4]
+  })
 
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
+  note.save().then(() => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+}
