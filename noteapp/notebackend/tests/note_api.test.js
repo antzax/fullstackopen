674c0aaf467bb811ad3e4d6812cd33c605a1e429
antzax <166389859+antzax@users.noreply.github.com>
2025-04-30 12:54:54 +0300

    add tests for creating new note

--- noteapp/notebackend/tests/note_api.test.js
+++ noteapp/notebackend/tests/note_api.test.js
@@ -4,39 +4,29 @@ const supertest = require('supertest')
 const app = require('../app')
 const assert = require('node:assert')
 const Note = require('../models/note')
+const helper = require('./test_helper')
 
 const api = supertest(app)
 
-const initialNotes = [
-  {
-    content: 'HTML is easy',
-    importance: false
-  },
-  {
-    content: 'Browser can excecute only Javascript',
-    important: true
-  }
-]
-
 beforeEach(async () => {
   await Note.deleteMany({})
-  let noteObject = new Note(initialNotes[0])
+  let noteObject = new Note(helper.initialNotes[0])
   await noteObject.save()
-  noteObject = new Note(initialNotes[1])
+  noteObject = new Note(helper.initialNotes[1])
   await noteObject.save()
 })
 
-test.only('notes are returned as json', async () => {
+test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
-test.only('all notes are returned', async () => {
+test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
 
-  assert.strictEqual(response.body.length, initialNotes.length)
+  assert.strictEqual(response.body.length, helper.initialNotes.length)
 })
 
 test('a specific note is within the returned notes', async () => {
@@ -44,6 +34,41 @@ test('a specific note is within the returned notes', async () => {
 
   const contents = response.body.map(e => e.content)
   assert(contents.includes('HTML is easy'))
+})
+
+test('a valid note can be added', async () => {
+  const newNote = {
+    content: 'this is the new note',
+    important: true
+  }
+
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  
+  const notesAtEnd = await helper.notesInDb()
+  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length + 1)
+
+  const contents = notesAtEnd.map(n => n.content)
+  assert(contents.includes('this is the new note'))
+})
+
+test('a note without content is not added', async () => {
+  const newNote = {
+    important: true
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(400)
+
+  const notesAtEnd = await helper.notesInDb()
+
+  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
 })
 
 after(async () => {
