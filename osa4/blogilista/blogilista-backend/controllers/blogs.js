35af99b11710610eaa641c899ce44089408e4161
antzax <166389859+antzax@users.noreply.github.com>
2025-05-02 01:46:59 +0300

    add userExtractor middleware

--- osa4/blogilista/blogilista-backend/controllers/blogs.js
+++ osa4/blogilista/blogilista-backend/controllers/blogs.js
@@ -11,13 +11,7 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const { title, author, url, likes } = request.body
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-
-  if(!decodedToken.id){
-    return response.status(401).json({ error: 'token invalid' })
-  }
-
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   const blog = new Blog({ title, author, url, likes, user: user._id })
 
@@ -32,17 +26,9 @@ blogsRouter.post('/', async (request, response) => {
 blogsRouter.delete('/:id', async (request, response) => {
   const id = request.params.id
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const user = request.user
 
-  if(!decodedToken.id) {
-    return response.status(401).json({ error: 'token invaid' })
-  }
-
-  const user = await User.findById(decodedToken.id)
   const blog = await Blog.findById(id)
-
-  console.log(blog.user)
-  console.log(user)
 
   if(blog.user.toString() === user.id.toString()) {
     await Blog.findByIdAndDelete(id)
