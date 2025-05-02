890d31f01e70e29c2494fb382b334922ca78e819
antzax <166389859+antzax@users.noreply.github.com>
2025-05-02 14:24:11 +0300

    add token handling for note service

--- noteapp/notefrontend/src/services/notes.js
+++ noteapp/notefrontend/src/services/notes.js
@@ -1,28 +1,34 @@
 import axios from 'axios'
 const baseUrl = '/api/notes'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  const nonExisting = {
-    id: 10000,
-    content: 'This note is not saved to server',
-    important: true,
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
+
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token }
   }
-  return request.then(response => response.data.concat(nonExisting))
+
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
 }
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
-}
-
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
+const update = async (id, newObject) => {
+  const response = await axios.put(`${baseUrl}/${id}`, newObject)
+  return response.data
 }
 
 export default { 
-  getAll: getAll, 
-  create: create, 
-  update: update 
+  getAll,
+  create,
+  update,
+  setToken
 }
