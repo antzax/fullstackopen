1e112edc6afe1446f1c4f82c996c6455d5a1e89f
antzax <166389859+antzax@users.noreply.github.com>
2025-05-04 22:17:32 +0300

    add placeholders for LoginForm input fields to help e2e locator query

--- osa5/blogilista/bloglist-frontend/src/components/LoginForm.jsx
+++ osa5/blogilista/bloglist-frontend/src/components/LoginForm.jsx
@@ -5,11 +5,11 @@ const LoginForm = ({ username, password, setUsername, setPassword, handleLogin }
       <form onSubmit={handleLogin}>
         <div>
           <label htmlFor="username">username</label>
-          <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} name="username" />
+          <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} name="username" placeholder="username"/>
         </div>
         <div>
           <label htmlFor="password">password</label>
-          <input type="password" value={password} onChange={({ target }) => setPassword(target.value)} name="password" />
+          <input type="password" value={password} onChange={({ target }) => setPassword(target.value)} name="password" placeholder="password"/>
         </div>
         <button type="submit">log in</button>
       </form>
