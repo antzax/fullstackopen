805e5e95be3a10f4238ad91fe71adcc76d0abf5a
antzax <166389859+antzax@users.noreply.github.com>
2025-05-04 22:16:00 +0300

    make e2e tests configuration and add first test

---
+++ osa5/blogilista/e2e-tests/tests/blog.spec.js
@@ -0,0 +1,21 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'root',
+        username: 'admin',
+        password: 'secret'
+      }
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByPlaceholder('username').first()).toBeVisible()
+    await expect(page.getByPlaceholder('password').last()).toBeVisible()
+  })
+})
